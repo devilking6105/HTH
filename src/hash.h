01ac202a1cb4d2c0890c11ba1265f4b761905ef4
barrystyle <barrystyle@westnet.com.au>
2019-07-10 13:31:59 +0800

    develop: implement new chain/genesis

--- src/hash.h
+++ src/hash.h
@@ -29,9 +29,8 @@
 #include "crypto/sph_fugue.h"
 #include "crypto/sph_shabal.h"
 #include "crypto/sph_whirlpool.h"
-extern "C" {
 #include "crypto/sph_sha2.h"
-}
+
 #include <vector>
 
 typedef uint256 ChainCode;
--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -51,10 +51,10 @@ static const int MAX_URI_LENGTH = 255;
 /* Number of frames in spinner animation */
 #define SPINNER_FRAMES 36
 
-#define QAPP_ORG_NAME "Dash"
-#define QAPP_ORG_DOMAIN "dash.org"
-#define QAPP_APP_NAME_DEFAULT "Dash-Qt"
-#define QAPP_APP_NAME_TESTNET "Dash-Qt-testnet"
-#define QAPP_APP_NAME_DEVNET "Dash-Qt-%s"
+#define QAPP_ORG_NAME "Helpthehomeless"
+#define QAPP_ORG_DOMAIN "helpthehomeless.org"
+#define QAPP_APP_NAME_DEFAULT "Helpthehomeless-Qt"
+#define QAPP_APP_NAME_TESTNET "Helpthehomeless-Qt-testnet"
+#define QAPP_APP_NAME_DEVNET "Helpthehomeless-Qt-%s"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
