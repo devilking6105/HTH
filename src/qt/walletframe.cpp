0cd2ef278c918cdfc4b009c0cfa27ea8c9cd6cfe
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-04-25 19:42:19 -0400

    Update WalletFrame.cpp
    Added OverviewAPage(PrivateSend) to WalletFrame.

--- src/qt/walletframe.cpp
+++ src/qt/walletframe.cpp
@@ -108,6 +108,20 @@ void WalletFrame::showOutOfSyncWarning(bool fShow)
         i.value()->showOutOfSyncWarning(fShow);
 }
 
+void WalletFrame::gotoAnnView()
+{
+    QMap<QString, WalletView*>::const_iterator i;
+    for (i = mapWalletViews.constBegin(); i != mapWalletViews.constEnd(); ++i)
+        i.value()->gotoAnnView();
+}
+
+void WalletFrame::gotoOverviewAPage()
+{
+    QMap<QString, WalletView*>::const_iterator i;
+    for (i = mapWalletViews.constBegin(); i != mapWalletViews.constEnd(); ++i)
+        i.value()->gotoOverviewAPage();
+}
+
 void WalletFrame::gotoOverviewPage()
 {
     QMap<QString, WalletView*>::const_iterator i;
