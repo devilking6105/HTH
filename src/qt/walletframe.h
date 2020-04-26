99d074a9030e34a233def64b3602b98551a9ecd7
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2020-04-25 21:55:23 -0400

    Update walletframe.h

--- src/qt/walletframe.h
+++ src/qt/walletframe.h
@@ -65,8 +65,6 @@ public Q_SLOTS:
 
     /** Switch to private send page */
     void gotoOverviewAPage();
-    /** Switch to announcement page */
-    void gotoAnnView();
     /** Switch to overview (home) page */
     void gotoOverviewPage();
     /** Switch to history (transactions) page */
